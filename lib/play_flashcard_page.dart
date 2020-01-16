25599fe64763d54bcb624b88ffc2213461c37f58
dygl2 <t0m013h@gmail.com>
2020-01-16 16:57:04 +0900

    [add]create play_flashcard_page.dart#1

---
+++ lib/play_flashcard_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PlayFlashcardPage extends StatefulWidget {
+  @override
+  _PlayFlashcardPageState createState() => _PlayFlashcardPageState();
+}
+
+class _PlayFlashcardPageState extends State<PlayFlashcardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
